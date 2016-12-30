dc675c7817b551506939bab507a61df294d08467
Blak3Nick <blaken2021@gmail.com>
2016-12-30 11:35:45 -0500

    continuing to work on the block breaker game

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -2,10 +2,17 @@
 using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
-
+	static MusicPlayer instance = null;
 	// Use this for initialization
 	void Start () {
-		GameObject.DontDestroyOnLoad(gameObject);
+		if (instance != null) {
+			Destroy(gameObject);
+			print ("Duplicate music player destroyed!!!");
+		}else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+		
 	}
 	
 	// Update is called once per frame
